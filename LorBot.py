d150ab4d0454011379af8ded0548318f35352dab
Lorek <lorek.adam2@gmail.com>
2022-03-25 00:40:34 +0100

    Zmiana tokena

--- LorBot.py
+++ LorBot.py
@@ -15,4 +15,4 @@ bot.load_extension("lastVersionCommands")
 bot.load_extension("twistedFateDiscord")
 #bot.load_extension('testcommands')
 
-bot.run('ODU0MzA5MjkyMDcyNTAxMjc5.YMiDhw._y1r5lxMuZ4eXR-pSyV5xroLLdo')
+bot.run('TOKEN')
